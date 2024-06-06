4a08d64b53375e201b0976717332fc6fd5f95928
ngocaoquyen <ngocaoquyen2000@gmail.com>
2024-06-06 14:47:43 +0700

    update

--- Api/Dtos/MovieSeries/UpdateMovieSeriesDto.cs
+++ Api/Dtos/MovieSeries/UpdateMovieSeriesDto.cs
@@ -4,10 +4,8 @@
     {
         public string Name { get; set; }
         public string? Description { get; set; }
-        public string? PosterPath { get; set; }
         public IFormFile ImageFile { get; set; }
         public int TotalEpisode { get; set; }
-        public int TotalNumberofViewers { get; set; }
         public int RegionId { get; set; }
         public int YearId { get; set; }
         public int[] GenreIds { get; set; }
